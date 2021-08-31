879b2bb15c2c94b5734463a3619733c371503848
Himanshu Bansal <himanshubansal155@gmail.com>
2021-08-31 18:56:41 +0530

    Created done page and done button to add

--- src/pages/Done.page.tsx
+++ src/pages/Done.page.tsx
@@ -1,11 +1,31 @@
-import {FC, memo} from "react";
+import { FC, memo, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { LS_INBOX } from "../actions/action.constants";
+import { todoFetchAction } from "../actions/todoList.actions";
+import TodoComponent from "../components/Todo.component";
+import {
+  todoListDataSelector,
+  todoListDoneSelector,
+} from "../selectors/todoList.selectors";
+import { useAppSelector } from "../store";
 
 interface Props {}
 
 const Done: FC<Props> = () => {
+  const done = useAppSelector(todoListDoneSelector);
+  const data1 = useAppSelector(todoListDataSelector);
+  useEffect(() => {
+    const data = localStorage.getItem(LS_INBOX);
+    data && dispatch(todoFetchAction(JSON.parse(data)));
+  }, []); //eslint-disable-line
+  const dispatch = useDispatch();
+
   return (
     <div>
-        Done page
+      {done &&
+        done.map((element) => (
+          <TodoComponent data={data1[element]} id={element} key={element} />
+        ))}
     </div>
   );
 };
