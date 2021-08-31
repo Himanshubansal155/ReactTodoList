7c93dddca26b7d99b75da328e45a18767d6dfc6a
Himanshu Bansal <himanshubansal155@gmail.com>
2021-08-31 18:57:06 +0530

    Created Important page and add all its implementations

--- src/pages/Important.page.tsx
+++ src/pages/Important.page.tsx
@@ -1,11 +1,28 @@
-import {FC, memo} from "react";
+import {FC, memo, useEffect} from "react";
+import { useDispatch } from "react-redux";
+import { LS_INBOX } from "../actions/action.constants";
+import { todoFetchAction } from "../actions/todoList.actions";
+import TodoComponent from "../components/Todo.component";
+import { todoListImportantSelector, todoListDataSelector } from "../selectors/todoList.selectors";
+import { useAppSelector } from "../store";
 
 interface Props {}
 
 const Important: FC<Props> = () => {
+  const done = useAppSelector(todoListImportantSelector);
+  const data1 = useAppSelector(todoListDataSelector);
+  useEffect(() => {
+    const data = localStorage.getItem(LS_INBOX);
+    data && dispatch(todoFetchAction(JSON.parse(data)));
+  }, []); //eslint-disable-line
+  const dispatch = useDispatch();
+
   return (
     <div>
-        Important page
+      {done &&
+        done.map((element) => (
+          <TodoComponent data={data1[element]} id={element} key={element} />
+        ))}
     </div>
   );
 };
